2b5f7922c4ba575e43287b952b390a46b1dc1825
viacheslavk55 <525vkostenko@frhsd.com>
2024-12-06 11:38:31 -0500

    Creating the vue app

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inventory: 100
+            product: 'Socks'
         }
     }
 })
