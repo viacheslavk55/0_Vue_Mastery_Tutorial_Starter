2cfe43ea874d6924f533d9d72ed4d0c5b198818e
viacheslavk55 <525vkostenko@frhsd.com>
2024-12-06 11:19:09 -0500

    Conditional Rendering

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            inventory: 100
+        }
+    }
+})
