ad492c09bfef7fb5e4119e0a7b721786f984cd14
viacheslavk55 <525vkostenko@frhsd.com>
2024-12-10 09:09:26 -0500

    Computed Properties

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
 const app = Vue.createApp({
-    data() {
-        return {
+    data: function() {
+        return{
+            cart:0,
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quanity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quanity: 0 },
               ]
+            
         }
     },
     methods: {
         addToCart() {
             this.cart += 1;
         },
-        updateImage(variantImage) {
-            this.image = variantImage;
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quanity
         }
     }
 })
